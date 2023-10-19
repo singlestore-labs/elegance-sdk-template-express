de2015c24b06a0e2dba59c2038306d9255a8089d
demenskyi <yaroslav.demenskiy@gmail.com>
2023-10-19 15:38:49 -0700

    kai template added

--- client/src/services/eleganceClient.ts
+++ client/src/services/eleganceClient.ts
@@ -1,5 +1,5 @@
 import { createEleganceClient } from "@singlestore/elegance-sdk";
 
-export const eleganceClient = createEleganceClient("mysql", {
+export const eleganceClient = createEleganceClient("kai", {
   baseURL: `http://localhost:${process.env.REACT_APP_SERVER_PORT}/api`
 });
--- server/src/services/eleganceServerClient.ts
+++ server/src/services/eleganceServerClient.ts
@@ -1,11 +1,9 @@
 import { createEleganceServerClient } from "@singlestore/elegance-sdk/server";
 
-export const eleganceServerClient = createEleganceServerClient("mysql", {
+export const eleganceServerClient = createEleganceServerClient("kai", {
   connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    uri: process.env.DB_URI ?? "",
+    database: process.env.DB_NAME ?? ""
   },
   openai: {
     apiKey: process.env.OPENAI_API_KEY
