2b6143f798706496f033753f716589c7b30e180a
Marta Vasconcelos <mvasconcelos@singlestore.com>
2024-07-23 15:17:31 +0100

    add shared tier compatibility

--- server/src/services/eleganceServerClient.ts
+++ server/src/services/eleganceServerClient.ts
@@ -1,11 +1,22 @@
 import { createEleganceServerClient } from "@singlestore/elegance-sdk/server";
 
+let cert;
+
+fetch("https://portal.singlestore.com/static/ca/singlestore_bundle.pem")
+  .then(function (response) {
+    response.text().then(function (text) {
+      cert = text;
+    });
+  })
+
 export const eleganceServerClient = createEleganceServerClient("mysql", {
   connection: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT),
+    ssl: { cert }
   },
   ai: {
     openai: {
